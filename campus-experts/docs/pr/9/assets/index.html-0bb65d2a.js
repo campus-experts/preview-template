88c2bb6bc35e5e4e1378c9aa2a9a9531ffc79691
j0ashm <j0ashm@users.noreply.github.com>
2023-08-12 20:25:30 +0000

    ci: deploy preview for campus-experts/docs/pr/9
    Commit created with [EndBug/pages-preview](https://github.com/EndBug/pages-preview)

---
+++ campus-experts/docs/pr/9/assets/index.html-0bb65d2a.js
@@ -0,0 +1,1 @@
+const e=JSON.parse('{"key":"v-147825fb","path":"/docs/","title":"","lang":"en-US","frontmatter":{},"headers":[],"git":{"updatedTime":1691871825000,"contributors":[{"name":"Joash Mathew","email":"25700330+j0ashm@users.noreply.github.com","commits":1}]},"filePathRelative":"docs/README.md"}');export{e as data};
