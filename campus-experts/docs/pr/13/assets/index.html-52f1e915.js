4ce5e3455734e27cfc3e1b3c10022e2d06f49037
juanpflores <juanpflores@users.noreply.github.com>
2023-09-22 20:18:37 +0000

    ci: remove preview for campus-experts/docs/pr/13
    Commit created with [EndBug/pages-preview](https://github.com/EndBug/pages-preview)

--- campus-experts/docs/pr/13/assets/index.html-52f1e915.js
+++
@@ -1,1 +0,0 @@
-const e=JSON.parse('{"key":"v-147825fb","path":"/docs/","title":"","lang":"en-US","frontmatter":{},"headers":[],"git":{"updatedTime":1695407945000,"contributors":[{"name":"Joash Mathew","email":"25700330+j0ashm@users.noreply.github.com","commits":1}]},"filePathRelative":"docs/README.md"}');export{e as data};
