d4b13496ffba194bc20c95282e548569ee86bdcd
juanpflores <juanpflores@users.noreply.github.com>
2023-10-12 21:07:04 +0000

    ci: deploy preview for campus-experts/docs/pr/18
    Commit created with [EndBug/pages-preview](https://github.com/EndBug/pages-preview)

---
+++ campus-experts/docs/pr/18/assets/index.html-818662a8.js
@@ -0,0 +1,1 @@
+const e=JSON.parse('{"key":"v-147825fb","path":"/docs/","title":"","lang":"en-US","frontmatter":{},"headers":[],"git":{"updatedTime":1697144665000,"contributors":[{"name":"Juan Pablo Flores","email":"juanpflores@github.com","commits":1}]},"filePathRelative":"docs/README.md"}');export{e as data};
