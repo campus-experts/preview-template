f48e01d0a34cabc223f180f1605da85bedb8cf0c
EndBug <EndBug@users.noreply.github.com>
2025-08-11 00:31:08 +0000

    chore: cleanup old preview directories
    Removed preview directories for branches and PRs that no longer exist.
    Commit created with [EndBug/pages-preview](https://github.com/EndBug/pages-preview)

--- campus-experts/campus-experts.github.io/pr/4537/jest.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} */
-module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  globalSetup: '<rootDir>/_scripts/jest-global-setup.ts',
-  setupFilesAfterEnv: ['<rootDir>/_scripts/jest-setup.ts'],
-};
