b2f2b69a21540d566573a471fc3ce18c6bc8eb39
EndBug <EndBug@users.noreply.github.com>
2025-08-11 18:38:17 +0000

    ci: deploy preview for campus-experts/campus-experts.github.io/pr/3015
    Commit created with [EndBug/pages-preview](https://github.com/EndBug/pages-preview)

--- campus-experts/campus-experts.github.io/pr/3015/jest.config.js
+++ campus-experts/campus-experts.github.io/pr/3015/jest.config.js
@@ -1,26 +1,17 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} */
-module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  globalSetup: '<rootDir>/_scripts/jest-global-setup.ts',
-  setupFilesAfterEnv: ['<rootDir>/_scripts/jest-setup.ts'],
-  reporters:
-    process.env.CI === 'true'
-      ? [
-          [
-            '@endbug/jest-pr-reporter',
-            {
-              githubToken: process.env.GH_TOKEN,
-              owner: process.env.GITHUB_REPOSITORY_OWNER,
-              repo:
-                process.env.GITHUB_REPOSITORY &&
-                process.env.GITHUB_REPOSITORY.split('/')[1],
-              prNumber: Number(process.env.PR_NUMBER),
-              sha: process.env.GITHUB_SHA,
-              workspace: process.env.GITHUB_WORKSPACE,
-              workflowRunId: process.env.GITHUB_RUN_ID,
-              jobName: process.env.GITHUB_JOB,
-              footerFailed: `## 🛠️ Next Steps
+/** @type {import('@endbug/jest-pr-reporter').ReporterOptions} */
+const reporterOptions = {
+  githubToken: process.env.GH_TOKEN,
+  owner: process.env.GITHUB_REPOSITORY_OWNER,
+  repo:
+    process.env.GITHUB_REPOSITORY &&
+    process.env.GITHUB_REPOSITORY.split('/')[1],
+  prNumber: Number(process.env.PR_NUMBER),
+  sha: process.env.GITHUB_SHA,
+  workspace: process.env.GITHUB_WORKSPACE,
+  workflowRunId: process.env.GITHUB_RUN_ID,
+  jobName: process.env.GITHUB_JOB,
+  customHeader: 'jest-tests',
+  footerFailed: `## 🛠️ Next Steps
 
 1. **Review** the failing tests above
 2. **Fix** your files so that the tests pass
@@ -29,9 +20,16 @@ module.exports = {
 > [!TIP]
 > Need help? Check the [README](https://github.com/campus-experts/campus-experts.github.io#readme) or ask in the Discord.
 `.trim(),
-            },
-          ],
-          'summary',
-        ]
+};
+
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  globalSetup: '<rootDir>/_scripts/jest-global-setup.ts',
+  setupFilesAfterEnv: ['<rootDir>/_scripts/jest-setup.ts'],
+  reporters:
+    process.env.CI === 'true'
+      ? [['@endbug/jest-pr-reporter', reporterOptions], 'summary']
       : ['default'],
 };
